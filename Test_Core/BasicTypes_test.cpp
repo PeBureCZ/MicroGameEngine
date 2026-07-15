97fa09116bf4ffcca4a26d5a32473aed62fb6903
Bures <bures.peter@email.cz>
2026-07-15 22:11:47 +0200

    add Unit testing projects

--- Test_Core/BasicTypes_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-#include "..\MGE_core\include\BasicTypes.h"
-
-TEST(BasicTypes, Default)
-{
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
-}
---
+++ Test_Gui/Widget_test.cpp
@@ -0,0 +1,62 @@
+#include "pch.h"
+
+#include "..\MGE_GUI\include\Widget.h"
+
+constexpr IPoint startTestPosition(23,785);
+constexpr mgeType::Size<int> startTestSize(201,335);
+
+class Widget_test : public Widget
+{
+public:
+	Widget_test() : Widget(startTestPosition, startTestSize)
+	{
+
+	}
+
+	std::weak_ptr<Widget> mock_getSelfPtr() const noexcept
+	{
+		return getSelfPtr();
+	}
+
+};
+
+TEST(Widgets, ptr)
+{
+	auto widget = std::make_shared< Widget_test>();
+	guiAdd::addWidgetToGui(widget);
+	bool testCase = widget->mock_getSelfPtr().lock() ? true : false;
+	EXPECT_TRUE(testCase);
+
+	EXPECT_EQ(widget->getAbsolutePosition(), startTestPosition);
+}
+
+TEST(Widgets, basicLocation)
+{
+	auto widget = std::make_shared< Widget_test>();
+	EXPECT_EQ(widget->getAbsolutePosition(), startTestPosition);
+
+	constexpr IPoint testLocation1(77,44);
+	widget->setRelativePosition(testLocation1);
+	EXPECT_EQ(widget->getAbsolutePosition(), testLocation1);
+	EXPECT_EQ(widget->getRelativePosition(), testLocation1);
+}
+TEST(Widgets, parentLocation)
+{
+	auto widget_child = std::make_shared< Widget_test>();
+	auto widget_parent = std::make_shared< Widget_test>();
+
+
+	constexpr IPoint testLocation(10, 10);
+	widget_parent->setRelativePosition(testLocation);
+	widget_child->setRelativePosition(testLocation);
+
+	guiAdd::addWidgetToGui(widget_parent);
+	guiAdd::addWidgetToGui(std::dynamic_pointer_cast<Widget>(widget_parent), std::dynamic_pointer_cast<Widget>(widget_child));
+
+
+	EXPECT_EQ(widget_parent->getAbsolutePosition(), testLocation);
+	EXPECT_EQ(widget_parent->getRelativePosition(), testLocation);
+
+	EXPECT_EQ(widget_child->getAbsolutePosition(), testLocation * 2);
+	EXPECT_EQ(widget_child->getRelativePosition(), testLocation);
+}
